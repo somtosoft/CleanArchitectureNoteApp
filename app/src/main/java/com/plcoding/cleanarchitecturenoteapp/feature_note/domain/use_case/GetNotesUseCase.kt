41b9fcd96e2e522b83e0f72614adf1d42b633ede
a025073m <a025073m@student.staffs.ac.uk>
2023-03-28 19:55:12 +0100

    Added Notes database and classes

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -0,0 +1,13 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetNotesUseCase(
+    private val repository:NoteRepository
+) {
+}
